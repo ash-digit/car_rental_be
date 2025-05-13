c5c1f9a3e06b87a79d0c57dcb4bbc264eec06d1e
ash-digit <ashdigit44@gmail.com>
2025-05-13 11:58:42 +0100

    initial commit

---
+++ src/main/java/com/example/car_rental_back_end/CarRentalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_rental_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalBackEndApplication.class, args);
+	}
+
+}
