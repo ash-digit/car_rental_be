59cf1fbe7bb82f9cff067101628fb1b2374516ab
edpau <edpau@me.com>
2025-05-13 14:31:50 +0100

    add customer model

--- src/main/java/com/example/car_rental_back_end/models/Customer.java
+++ src/main/java/com/example/car_rental_back_end/models/Customer.java
@@ -1,4 +1,49 @@
 package com.example.car_rental_back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity (name = "Customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToOne(mappedBy = "customer")
+    @JsonIgnore
+    private Booking booking;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
 }
