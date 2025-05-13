73f88d713eac3e1681ba2b735afe39ee326f5ee7
nsamuels <n-samuels@outlook.com>
2025-05-13 15:09:42 +0100

    adding Booking model

--- src/main/java/com/example/car_rental_back_end/models/Booking.java
+++ src/main/java/com/example/car_rental_back_end/models/Booking.java
@@ -1,4 +1,80 @@
 package com.example.car_rental_back_end.models;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity(name = "Bookings")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private Integer totalPrice;
+    private Boolean bookingStatus;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Boolean getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(Boolean bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
