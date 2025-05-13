902544c48aa10dbe1fe79a42425e4f2ebb508dc1
jonnyell89 <jonnyell89@gmail.com>
2025-05-13 14:49:09 +0100

    added car model

--- src/main/java/com/example/car_rental_back_end/models/Car.java
+++ src/main/java/com/example/car_rental_back_end/models/Car.java
@@ -1,4 +1,76 @@
 package com.example.car_rental_back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity(name = "Cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean availability;
+    private String make;
+    private String model;
+    private Integer year;
+    private Integer dailyRate;
+
+    @OneToOne(mappedBy = "car")
+    @JsonIgnore
+    private Booking booking;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getDailyRate() {
+        return dailyRate;
+    }
+
+    public void setDailyRate(Integer dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
 }
